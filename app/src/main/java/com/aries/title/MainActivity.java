98dcf4b546d6daddcb3a6ed335845de6c96bdc18
Aries <hutao19930614@foxmail.com>
2017-07-12 22:51:34 +0800

    1、增加TitleBarView 切换沉浸与否方法

--- app/src/main/java/com/aries/title/MainActivity.java
+++ app/src/main/java/com/aries/title/MainActivity.java
@@ -3,7 +3,6 @@ package com.aries.title;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.aries.ui.view.title.TitleBarView;
 
@@ -21,7 +20,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         titleBar = (TitleBarView) findViewById(R.id.titleBar);
-        titleBar.setImmersible(MainActivity.this,true);
         initView();
     }
 
@@ -29,15 +27,15 @@ public class MainActivity extends Activity {
         titleBar.setOnLeftTextClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "点击左边", Toast.LENGTH_SHORT).show();
+                titleBar.setImmersible(MainActivity.this, true);
             }
         });
-        titleBar.addRightAction(titleBar.new ImageAction(R.drawable.ic_arrow_right, new View.OnClickListener() {
+        titleBar.setOnRightTextClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "点击", Toast.LENGTH_SHORT).show();
+                titleBar.setImmersible(MainActivity.this, false);
             }
-        }));
+        });
 
 
     }
